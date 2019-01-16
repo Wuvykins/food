b6a7727cc9fae027760c79f2fd4ae4fb8f7b201b
Nic Alexander <wuvykins@gmail.com>
2019-01-16 16:01:34 -0500

    in class stuff

--- index.php
+++ index.php
@@ -58,5 +58,75 @@ $f3->route('GET /dinner/rolls', function(){ //get method is the default to go to
     echo $view->render('views/rolls.html');
 });
 
+//define a route with a parameter
+$f3->route('Get /@food', function($f3, $params)
+{
+    print_r($params);
+    echo "<h3>I like" . $params['food'] . "</h3>";
+});
+
+$f3->route('Get /@meal/@food', function($f3, $params)
+{
+    print_r($params);
+    $validMeals = ['breakfast', 'lunch', 'dinner'];
+    $meal = $params['meal'];
+    //check validity
+    if (!in_array($meal, $validMeals))
+    {
+        echo "<h3>Sorry, we don't serve $meal</h3>";
+    }
+    else
+    {
+        switch (meal)
+        {
+            case 'breakfast': $time = " in the morning"; break;
+            case 'lunch': $time = " at noon"; break;
+            case 'dinner': $time = " in the evening"; break;
+        }
+        echo "<h3>I like " . $params['food'] . " $time</h3>";
+    }
+});
+
+//define a route to display order form
+$f3->route('Get /order', function()
+{
+    $view = new View();
+    echo $view->render('views/form1.html');
+});
+
+//define an order to process orders
+$f3->route('POST /order-process', function($f3)
+{
+    print_r($_POST);
+
+    $food = $_POST['food'];
+    echo "you ordered food";
+    if ($food == 'pizza')
+    {
+        //reroute to pizza page
+        $f3->reroute("pizza");
+    }
+    else
+    {
+        $f3->reroute("");
+    }
+
+
+});
+
+$f3->route('Get /dessert/@param', function($f3, $params)
+{
+    $params = $params["params"];
+    if ($params == "pie")
+    {
+        echo "<h3>I like pie</h3>";
+    }
+    else
+    {
+        $f3->error(404);
+    }
+
+});
+
 //Run fat free
 $f3->run();
